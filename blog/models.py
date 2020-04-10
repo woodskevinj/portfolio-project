24e08c5838c80f89adc241e65fdcbfa7ae554e60
kevinwoods <woodskevinj@gmail.com>
2020-04-10 06:45:32 -0400

    Completed my django site

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,18 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(null=True)
     body = models.TextField(null=True)
     image = models.ImageField(upload_to='images/', null=True)
+
+
+    # change admin view to show actual title not "blog object"
+    def __str__(self):
+        return self.title
+
+    # create custom method to show preview only for body
+
+    def summary(self):
+        return self.body[:100]
+
+    # create custom method to show date only
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
